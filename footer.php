c9f40e015bab127f1b4ed783dbe09e976bb4bb22
G-Apxo <72552435+G-Apxo@users.noreply.github.com>
2021-11-10 01:02:21 +0400

    asd

--- footer.php
+++ footer.php
@@ -88,3 +88,24 @@
     </div>
   </footer>
 </div>
+
+<!-- End Page Wrapper --> 
+
+<!-- JavaScripts --> 
+<script src="js/vendors/jquery/jquery.min.js"></script> 
+<script src="js/vendors/wow.min.js"></script> 
+<script src="js/vendors/bootstrap.min.js"></script> 
+<script src="js/vendors/own-menu.js"></script> 
+<script src="js/vendors/flexslider/jquery.flexslider-min.js"></script> 
+<script src="js/vendors/jquery.countTo.js"></script> 
+<script src="js/vendors/jquery.isotope.min.js"></script> 
+<script src="js/vendors/jquery.bxslider.min.js"></script> 
+<script src="js/vendors/owl.carousel.min.js"></script> 
+<script src="js/vendors/jquery.sticky.js"></script> 
+
+<!-- SLIDER REVOLUTION 4.x SCRIPTS  --> 
+<script type="text/javascript" src="rs-plugin/js/jquery.themepunch.tools.min.js"></script> 
+<script type="text/javascript" src="rs-plugin/js/jquery.themepunch.revolution.min.js"></script> 
+<script src="js/main.js"></script> 
+</body>
+</html>
---
+++ uni.php
@@ -0,0 +1,236 @@
+<!-- Header -->
+<?php include_once 'header.php';?>
+<!-- End Header --> 
+<!-- Content -->
+<div id="content">
+   <!-- Seo Secore -->
+   <section class="bg-parallax seo-secore padding-top-100 padding-bottom-50" no-repeat;">
+      <div class="container">
+         <!-- Tittle -->
+         <div class="heading-block text-dark text-center margin-bottom-50">
+            <h2 class="text-dark">What’s Your SEO Score ?</h2>
+            <span>See how well your page is optimised for your keyword</span> 
+         </div>
+         <!-- Form -->
+         <form>
+            <ul class="row">
+               <li class="col-md-4">
+                  <div class="form-group">
+                     <label for="exampleFormControlSelect1">ფაკულტეტი</label>
+                     <select class="form-control" id="exampleFormControlSelect1">
+                        <option>კომპიუტერული მეცნიერება</option>
+                        <option>ფსიქოლოგია</option>
+                        <option>სოციოლოგია</option>
+                        <option>სამედიცინო</option>
+                        <option>ფინანსები</option>
+                     </select>
+                  </div>
+               </li>
+               <li class="col-md-4">
+                  <div class="form-group">
+                     <label for="exampleFormControlSelect1">ლოკაცია</label>
+                     <select class="form-control" id="exampleFormControlSelect1">
+                        <option>თბილისი</option>
+                        <option>ქუთაისი</option>
+                        <option>გორი</option>
+                        <option>ბათუმი</option>
+                        <option>ხაშური</option>
+                     </select>
+                  </div>
+               </li>
+               <li class="col-md-4 margin-top-20">
+                  <button type="submit" class="btn btn-orange">ძებნა</button>
+               </li>
+            </ul>
+         </form>
+      </div>
+   </section>
+   <!-- Google Front Page -->
+   <section class="front-page padding-top-50 padding-bottom-100">
+      <div class="container padding-bottom-50 padding-top-50">
+         <div class="row" style="justify-content: center;display: flex;border-left: solid 9px #91181b;">
+            <div class="col-md-4" style="display: flex;justify-content: center;align-items: center;"> <img class="img-responsive " src="https://upload.wikimedia.org/wikipedia/commons/7/74/Iliaunilogo.png" alt=" " style="width:70%"> </div>
+            <div class="col-md-8">
+               <!-- Tittle -->
+               <div class="heading-block text-left" style="margin-bottom:5px">
+                  <h2>ილიას სახელმწიფო უნივერსიტეტი </h2>
+               </div>
+               <div class="heading-block text-left margin-bottom-20">
+                  <span>თბილისი </span>
+               </div>
+               <!-- List Style -->
+               <div class="row">
+                  <div class="col-md-6">
+                     <ul class="list-style">
+                        <li>
+                           <p><img src="images/price-tag.png" alt="" style="width:10%"> სწავლის საფასური: </p>
+                        </li>
+                        <li>
+                           <p><img src="images/wallet.png" alt="" style="width:10%"> საშუალო ხელფასი:</p>
+                        </li>
+                     </ul>
+                  </div>
+                  <div class="col-md-6">
+                     <ul class="list-style">
+                        <li>
+                           <p><img src="images/check.png" alt=""style="width:10%">ჩარიცხვის %: </p>
+                        </li>
+                        <li>
+                           <p><img src="images/graduation-cap.png" alt="" style="width:10%"> კურსდამთავრებულების %: </p>
+                        </li>
+                     </ul>
+                  </div>
+               </div>
+               <!-- Buttons --> 
+               <div class="col-md-12">
+                  <div class="row">
+                     <div class="col-md-6">
+                     </div>
+                     <div class="col-md-6" style="display: flex;justify-content: end;">
+                        <a href="#." class="btn margin-top-20">Learn More</a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="row margin-top-100" style="justify-content: center;display: flex;border-left: solid 9px #0066b1;">
+            <div class="col-md-4" style="display: flex;justify-content: center;align-items: center;"> <img class="img-responsive " src="https://upload.wikimedia.org/wikipedia/ka/d/d9/TSU_Coat_of_Arms.svg?download" alt=" " style="width:70%"> </div>
+            <div class="col-md-8">
+               <!-- Tittle -->
+               <div class="heading-block text-left" style="margin-bottom:5px">
+                  <h2>ივანე ჯავახიშვილის სახელობის თბილისის სახელმწიფო უნივერსიტეტი </h2>
+               </div>
+               <div class="heading-block text-left margin-bottom-20">
+                  <span>თბილისი </span>
+               </div>
+               <!-- List Style -->
+               <div class="row">
+                  <div class="col-md-6">
+                     <ul class="list-style">
+                        <li>
+                           <p><img src="images/price-tag.png" alt="" style="width:10%"> სწავლის საფასური: </p>
+                        </li>
+                        <li>
+                           <p><img src="images/wallet.png" alt="" style="width:10%"> საშუალო ხელფასი:</p>
+                        </li>
+                     </ul>
+                  </div>
+                  <div class="col-md-6">
+                     <ul class="list-style">
+                        <li>
+                           <p><img src="images/check.png" alt=""style="width:10%">ჩარიცხვის %: </p>
+                        </li>
+                        <li>
+                           <p><img src="images/graduation-cap.png" alt="" style="width:10%"> კურსდამთავრებულების %: </p>
+                        </li>
+                     </ul>
+                  </div>
+               </div>
+               <!-- Buttons --> 
+               <div class="col-md-12">
+                  <div class="row">
+                     <div class="col-md-6">
+                     </div>
+                     <div class="col-md-6" style="display: flex;justify-content: end;">
+                        <a href="#." class="btn margin-top-20">Learn More</a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="row margin-top-100" style="justify-content: center;display: flex;border-left: solid 9px #fecd2a;">
+            <div class="col-md-4" style="display: flex;justify-content: center;align-items: center;"> <img class="img-responsive " src="https://scontent.fkut1-1.fna.fbcdn.net/v/t1.6435-9/41442517_10156738822387700_5829195644958408704_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=2ESnbtUO_UkAX-F-Sih&_nc_ht=scontent.fkut1-1.fna&oh=59380f4c3a45840ccb975c6507298d93&oe=61B03CF0" alt=" " style="width:70%"> </div>
+            <div class="col-md-8">
+               <!-- Tittle -->
+               <div class="heading-block text-left" style="margin-bottom:5px">
+                  <h2>თავისუფალი უნივერსიტეტი </h2>
+               </div>
+               <div class="heading-block text-left margin-bottom-20">
+                  <span>თბილისი </span>
+               </div>
+               <!-- List Style -->
+               <div class="row">
+                  <div class="col-md-6">
+                     <ul class="list-style">
+                        <li>
+                           <p><img src="images/price-tag.png" alt="" style="width:10%"> სწავლის საფასური: </p>
+                        </li>
+                        <li>
+                           <p><img src="images/wallet.png" alt="" style="width:10%"> საშუალო ხელფასი:</p>
+                        </li>
+                     </ul>
+                  </div>
+                  <div class="col-md-6">
+                     <ul class="list-style">
+                        <li>
+                           <p><img src="images/check.png" alt=""style="width:10%">ჩარიცხვის %: </p>
+                        </li>
+                        <li>
+                           <p><img src="images/graduation-cap.png" alt="" style="width:10%"> კურსდამთავრებულების %: </p>
+                        </li>
+                     </ul>
+                  </div>
+               </div>
+               <!-- Buttons --> 
+               <div class="col-md-12">
+                  <div class="row">
+                     <div class="col-md-6">
+                     </div>
+                     <div class="col-md-6" style="display: flex;justify-content: end;">
+                        <a href="#." class="btn margin-top-20">Learn More</a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="row margin-top-100" style="justify-content: center;display: flex;border-left: solid 9px #4ABFED;">
+            <div class="col-md-4" style="display: flex;justify-content: center;align-items: center;"> <img class="img-responsive " src="images/UNIK.svg" alt=" " style="width:65%"> </div>
+            <div class="col-md-8">
+               <!-- Tittle -->
+               <div class="heading-block text-left" style="margin-bottom:5px">
+                  <h2>UNIK - ქუთაისის უნივერსიტეტი </h2>
+               </div>
+               <div class="heading-block text-left margin-bottom-20">
+                  <span>ქუთაისი </span>
+               </div>
+               <!-- List Style -->
+               <div class="row">
+                  <div class="col-md-6">
+                     <ul class="list-style">
+                        <li>
+                           <p><img src="images/price-tag.png" alt="" style="width:10%"> სწავლის საფასური: </p>
+                        </li>
+                        <li>
+                           <p><img src="images/wallet.png" alt="" style="width:10%"> საშუალო ხელფასი:</p>
+                        </li>
+                     </ul>
+                  </div>
+                  <div class="col-md-6">
+                     <ul class="list-style">
+                        <li>
+                           <p><img src="images/check.png" alt=""style="width:10%">ჩარიცხვის %: </p>
+                        </li>
+                        <li>
+                           <p><img src="images/graduation-cap.png" alt="" style="width:10%"> კურსდამთავრებულების %: </p>
+                        </li>
+                     </ul>
+                  </div>
+               </div>
+               <!-- Buttons --> 
+               <div class="col-md-12">
+                  <div class="row">
+                     <div class="col-md-6">
+                     </div>
+                     <div class="col-md-6" style="display: flex;justify-content: end;">
+                        <a href="#." class="btn margin-top-20">Learn More</a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </section>
+</div>
+<!-- End Content --> 
+<!-- Footer -->
+<?php include_once 'footer.php';?>
