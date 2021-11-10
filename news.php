81f987fe947317020cd8a909d984f2a9913c9e89
G-Apxo <72552435+G-Apxo@users.noreply.github.com>
2021-11-10 14:21:53 +0400

    news page upload

---
+++ news.php
@@ -0,0 +1,140 @@
+<!-- Header -->
+<?php include_once 'header.php';?>
+<!-- End Header --> 
+<!-- Content -->
+<div id="content">
+   <!-- Seo Secore -->
+   <section class="bg-parallax seo-secore padding-top-100 padding-bottom-50" no-repeat;">
+      <div class="container">
+         <!-- Tittle -->
+         <div class="heading-block text-dark text-left margin-bottom-80">
+            <h2 class="text-dark" style="font-size:40px;">იპოვე<span style="color:#00a561; font-size:40px;"> გაცვლითი პროგრამები </span></h2>
+            
+         </div>
+         <!-- Form -->
+         <form>
+            <ul class="row">
+               <li class="col-md-4">
+                  <div class="form-group">
+                     <label for="exampleFormControlSelect1">მე ვარ</label>
+                     <select class="form-control" id="exampleFormControlSelect1">
+                        <option>ყველა</option>
+                        <option>სტუდენტი</option>
+                        <option>მაგისტრი</option>
+                        <option>მოსწავლე</option>
+                        <option>მკვლევარი</option>
+                        <option>80$</option>
+                     </select>
+                  </div>
+               </li>
+               <li class="col-md-4">
+                  <div class="form-group">
+                     <label for="exampleFormControlSelect1">ლოკაცია</label>
+                     <select class="form-control" id="exampleFormControlSelect1">
+                        <option>ყველა</option>
+                        <option>ამერიკა</option>
+                        <option>ევროპა</option>
+                        <option>აზია</option>
+                        <option>ავსტრალია</option>
+                     </select>
+                  </div>
+               </li>
+               <li class="col-md-4 margin-top-10">
+                  <button type="submit" class="btn btn-orange">ძებნა</button>
+               </li>
+            </ul>
+         </form>
+      </div>
+   </section>
+   <!-- Google Front Page -->
+   <section class=" latest-news padding-top-100 padding-bottom-100">
+      <div class="container"> 
+        
+        <!-- Tittle -->
+        <div class="heading-block text-center margin-bottom-50">
+          <h2 style="font-size:40px;"> სიახლეები</h2>
+        </div>
+        <!-- News -->
+        <div class="row"> 
+          <!-- News 1 -->
+          <div class="col-md-6"> <a href="#."> <img class="img-responsive" src="images/new-1.jpg" alt=""> </a>
+            <div class="news-detail">
+              <div class="row">
+                
+                <div class="col-md-12"> <a href="#."><h2 style="font-size: 22px;">ყლეობა მთვარი</h2></a>
+                <div class="row">
+                  <div class="col-md-6">
+
+                  </div>
+                    <div class="col-md-6 sections2"  style="display: flex;justify-content: end;">
+                    <!-- Buttons --> 
+                      <a href="#." class="btn margin-top-20">მეტის ნახვა</a>  
+                    </div>
+                </div> 
+                </div>
+              </div>
+            </div>
+          </div>
+          
+          <!-- News 2 -->
+          <div class="col-md-6"> <a href="#."> <img class="img-responsive" src="images/new-2.jpg" alt=""> </a>
+            <div class="news-detail">
+              <div class="row">
+                
+                <div class="col-md-12"> <a href="#."> <h2 style="font-size: 22px;">ყლეობა ოღონდ მეორე</h2></a>
+                <div class="row">
+                  <div class="col-md-6">
+
+                  </div>
+                    <div class="col-md-6 sections2"  style="display: flex;justify-content: end;">
+                    <!-- Buttons --> 
+                      <a href="#." class="btn margin-top-20">მეტის ნახვა</a>  
+                    </div>
+                </div> 
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 margin-top-100"> <a href="#."> <img class="img-responsive" src="images/new-2.jpg" alt=""> </a>
+            <div class="news-detail">
+              <div class="row">
+                
+                <div class="col-md-12"> <a href="#."> <h2 style="font-size: 22px;">ყლეობა ოღონდ მეორე</h2></a>
+                <div class="row">
+                  <div class="col-md-6">
+
+                  </div>
+                    <div class="col-md-6 sections2"  style="display: flex;justify-content: end;">
+                    <!-- Buttons --> 
+                      <a href="#." class="btn margin-top-20">მეტის ნახვა</a>  
+                    </div>
+                </div> 
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 margin-top-100"> <a href="#."> <img class="img-responsive" src="images/new-2.jpg" alt=""> </a>
+            <div class="news-detail">
+              <div class="row">
+                
+                <div class="col-md-12"> <a href="#."> <h2 style="font-size: 22px;">ყლეობა ოღონდ მეორე</h2></a>
+                <div class="row">
+                  <div class="col-md-6">
+
+                  </div>
+                    <div class="col-md-6 sections2"  style="display: flex;justify-content: end;">
+                    <!-- Buttons --> 
+                      <a href="#." class="btn margin-top-20">მეტის ნახვა</a>  
+                    </div>
+                </div> 
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+</div>
+<!-- End Content --> 
+<!-- Footer -->
+<?php include_once 'footer.php';?>
