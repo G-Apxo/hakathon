b378295837753909da1b99dac6c58562baa42229
G-Apxo <72552435+G-Apxo@users.noreply.github.com>
2021-11-10 18:48:46 +0400

    final update

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
         <ul>
           
           <!-- Slider 1 -->
-          <li data-transition="fade" data-slotamount="7" style="background-color:#EFEFEF"> <img src="images/banner.png" data-bgposition="right center"  data-bgfit="contain"  alt="" style="width:50%;background-size: contain;"/> 
+          <li data-transition="fade" data-slotamount="7" style="background-color:#EFEFEF"> <img src="images/banner.png" data-bgposition="right center"  data-bgfit="contain"  alt="" style="width:40%;background-size: contain;"/> 
             
             <!-- Layer -->
             <div class="tp-caption sft tp-resizeme font-extra-bold" 
@@ -175,7 +175,7 @@
     <section class="light-gray-bg front-page padding-top-100 padding-bottom-100">
       <div class="container">
         <div class="row">
-          <div class="col-md-4"> <img class="img-responsive margin-top-30" src="images/ipad.jpg" alt=" "> </div>
+          <div class="col-md-4"> <img class="img-responsive margin-top-30" src="images/uni.png" alt=" "> </div>
           <div class="col-md-8"> 
             <!-- Tittle -->
             <div class="heading-block text-left margin-bottom-20">
@@ -272,14 +272,14 @@
               
             </div>
          </div>
-      <div class="col-md-4" style="display: flex;justify-content: end;"> <img class="img-responsive margin-top-30" src="images/ipad.jpg" alt=" "> 
+      <div class="col-md-4" style="display: flex;justify-content: end;"> <img class="img-responsive margin-top-30" src="images/exchange.png" alt=" "> 
       </div>
    </div>
 </section>
 <section class="light-gray-bg front-page padding-top-100 padding-bottom-100">
       <div class="container">
         <div class="row">
-          <div class="col-md-4"> <img class="img-responsive margin-top-30" src="images/ipad.jpg" alt=" "> </div>
+          <div class="col-md-4"> <img class="img-responsive margin-top-30" src="images/non-gov.png" alt=" "> </div>
           <div class="col-md-8"> 
             <!-- Tittle -->
             <div class="heading-block text-left margin-bottom-20">
