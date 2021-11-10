8f326ce3b39aef26c34458033be9fb4266a169e0
Davit Spanderashvili <37460419+davitITguy@users.noreply.github.com>
2021-11-10 04:18:59 +0400

    somethign

--- exchange.php
+++ exchange.php
@@ -55,11 +55,11 @@
             <li class="col-md-6">
               <article> <a href="#"> 
               <div class="col-md-12" style="border: solid 2px #0049a5;padding-top: 35px;padding-bottom: 35px;border-radius: 35px;">
-               <div class="col-md-4" style="display: flex;justify-content: center;align-items: center;    padding-top: 30px;"> <img class="img-responsive " src="https://live.staticflickr.com/1770/42015795075_0007b251ac_b.jpg" alt=" " style="width:100%"> </div>
+               <div class="col-md-4" style="display: flex;justify-content: center;align-items: center;    padding-top: 20px;"> <img class="img-responsive " src="https://live.staticflickr.com/1770/42015795075_0007b251ac_b.jpg" alt=" " style="width:100%"> </div>
                <div class="col-md-8">
                   <!-- Tittle -->
                   <div class="heading-block text-left" style="margin-bottom:5px">
-                     <h2>საერთაშორისო კრედიტ მობილობა</h2>
+                     <h2 style="font-size:28px;">საერთაშორისო კრედიტ მობილობა</h2>
                   </div>
                   <div class="row">
                      <div class="col-md-6">
@@ -104,55 +104,164 @@
               </article>
             </li>
             
-            <!-- Case 2 -->
+
             <li class="col-md-6">
               <article> <a href="#"> 
-              <div class="col-md-12" >
-               <div class="col-md-4" style="display: flex;justify-content: center;align-items: center;"> <img class="img-responsive " src="https://scontent.fkut1-1.fna.fbcdn.net/v/t1.18169-9/13516706_508457376027922_6490481796783975719_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=TWeLECtUBgoAX-qk_go&_nc_ht=scontent.fkut1-1.fna&oh=ae6d488cc2ca622982c70b7ef633b766&oe=61AFEA95" alt=" " style="width:70%"> </div>
+              <div class="col-md-12" style="border: solid 2px #003DA5;padding-top: 35px;padding-bottom: 35px;border-radius: 35px;">
+               <div class="col-md-4" style="display: flex;justify-content: center;align-items: center;    padding-top: 45px;"> <img class="img-responsive " src="images/template/exchange-fullbright.png" alt=" " style="width:100%"> </div>
                <div class="col-md-8">
                   <!-- Tittle -->
                   <div class="heading-block text-left" style="margin-bottom:5px">
-                     <h2>PITA - ქუთაისის ახალგაზრდული ცენტრი </h2>
+                     <h2 style="font-size:28px;">Fulbright Graduate Student Program</h2>
                   </div>
                   <div class="row">
                      <div class="col-md-6">
                         <ul class="list-style">
                            <li>
-                              <p><img src="images/location.png" alt="" style="width:10%">ამერიკა</p>
-                           </li>
-                           <li>
-                              <p><img src="images/user.png" alt=""style="width:10%">მაგისტრი </p>
+                    
+                              <p><img src="images/location.png" alt="" style="width:10%"> ევროპა </p>
+               
                            </li>
                         </ul>
                      </div>
                      <div class="col-md-6">
                         <ul class="list-style">
-                           
+                           <li>
+                              <p><img src="images/user.png" alt=""style="width:10%">მაგისტრი</p>
+                           </li>
                         </ul>
                      </div>
                   </div>
                   <!-- Buttons --> 
-                 
-               </div>
-            </div>
-              </a>
-                <div class="case-detail">
-                <div class="col-md-12">
+                  <div class="col-md-12">
                      <div class="row">
                         <div class="col-md-6">
                         </div>
-                        <div class="col-md-6" style="display: flex;justify-content: end;">
-                           <a href="#." class="btn margin-top-20">გაიგე მეტი</a>
-                        </div>
+
                      </div>
                   </div>
+               </div>
+            </div>
+
+              </a>
+                <div class="case-detail">
+                
+                     
+                        
+                        <div class="col-md-12 padding-top-10" style="display:flex; justify-content:center; align-items:center" >
+                           <a href="#." class="btn margin-top-20" style="width: 50%;height:40px;display: flex;justify-content: center;align-items: center;color: white;">გაიგე მეტი</a>
+                        </div>
+                     
+                  
+                </div>
+              </article>
+            </li>
+ 
+          <!-- // -->
+          <li class="col-md-6">
+              <article> <a href="#"> 
+              <div class="col-md-12" style="border: solid 2px #00B8DE;padding-top: 35px;padding-bottom: 35px;border-radius: 35px;">
+               <div class="col-md-4" style="display: flex;justify-content: center;align-items: center;    padding-top: 18px;"> <img class="img-responsive " src="images/template/exchange-global.png" alt=" " style="width:100%"> </div>
+               <div class="col-md-8">
+                  <!-- Tittle -->
+                  <div class="heading-block text-left" style="margin-bottom:5px">
+                     <h2 style="font-size:28px;">Global Undergraduate Exchange Program</h2>
+                  </div>
+                  <div class="row">
+                     <div class="col-md-6">
+                        <ul class="list-style">
+                           <li>
+                    
+                              <p><img src="images/location.png" alt="" style="width:10%"> ევროპა </p>
+               
+                           </li>
+                        </ul>
+                     </div>
+                     <div class="col-md-6">
+                        <ul class="list-style">
+                           <li>
+                              <p><img src="images/user.png" alt=""style="width:10%">სტუდენტი</p>
+                           </li>
+                        </ul>
+                     </div>
+                  </div>
+                  <!-- Buttons --> 
+                  <div class="col-md-12">
+                     <div class="row">
+                        <div class="col-md-6">
+                        </div>
+
+                     </div>
+                  </div>
+               </div>
+            </div>
+
+              </a>
+                <div class="case-detail">
+                
+                     
+                        
+                        <div class="col-md-12 padding-top-10" style="display:flex; justify-content:center; align-items:center" >
+                           <a href="#." class="btn margin-top-20" style="width: 50%;height:40px;display: flex;justify-content: center;align-items: center;color: white;">გაიგე მეტი</a>
+                        </div>
+                     
+                  
                 </div>
               </article>
             </li>
             
-         
-          </ul>
-          
+
+            <li class="col-md-6">
+              <article> <a href="#"> 
+              <div class="col-md-12" style="border: solid 2px #00529C;padding-top: 35px;padding-bottom: 35px;border-radius: 35px;">
+               <div class="col-md-4" style="display: flex;justify-content: center;align-items: center;    padding-top: 17px;"> <img class="img-responsive " src="images/template/exchange-flex.png" alt=" " style="width:100%"> </div>
+               <div class="col-md-8">
+                  <!-- Tittle -->
+                  <div class="heading-block text-left" style="margin-bottom:5px">
+                     <h2 style="font-size:28px;">Future Leaders Exchange Program (FLEX)</h2>
+                  </div>
+                  <div class="row">
+                     <div class="col-md-6">
+                        <ul class="list-style">
+                           <li>
+                    
+                              <p><img src="images/location.png" alt="" style="width:10%"> ევროპა </p>
+               
+                           </li>
+                        </ul>
+                     </div>
+                     <div class="col-md-6">
+                        <ul class="list-style">
+                           <li>
+                              <p><img src="images/user.png" alt=""style="width:10%">მოსწავლე</p>
+                           </li>
+                        </ul>
+                     </div>
+                  </div>
+                  <!-- Buttons --> 
+                  <div class="col-md-12">
+                     <div class="row">
+                        <div class="col-md-6">
+                        </div>
+
+                     </div>
+                  </div>
+               </div>
+            </div>
+
+              </a>
+                <div class="case-detail">
+                
+                     
+                        
+                        <div class="col-md-12 padding-top-10" style="display:flex; justify-content:center; align-items:center" >
+                           <a href="#." class="btn margin-top-20" style="width: 50%;height:40px;display: flex;justify-content: center;align-items: center;color: white;">გაიგე მეტი</a>
+                        </div>
+                     
+                  
+                </div>
+              </article>
+            </li>
           <!-- Button -->
          
         </div>
